1f65b02905228bf2af2d828dcbd4789b9a149fde
Sarathi V M <151382751+Sarathi-VM@users.noreply.github.com>
2024-10-09 22:31:38 +0530

    342- power-of-four

---
+++ power-of-four.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public boolean isPowerOfFour(int n) {
+        for (int i = 0; i <= 15; i++) {
+            int pow = (int) Math.pow(4, i);
+            if (pow==n)
+                return true;
+            if (pow>n)
+                return false;
+        }
+        return false;
+    }
+}
